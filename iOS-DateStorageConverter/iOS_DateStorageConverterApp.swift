13b53a495ccc3f16d2425903dc44492551a9547f
Devon Hanses <dhansen@lcs.on.ca>
2021-02-12 11:10:57 -0500

    Added iOS target using SwiftUI framework to project.

---
+++ iOS-DateStorageConverter/iOS_DateStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DateStorageConverterApp.swift
+//  iOS-DateStorageConverter
+//
+//  Created by Devon Hansen on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DateStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
